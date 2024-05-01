e66f9ca8a4007502cf9e0cc1c37a71ac254773e0
w3schools-test <test@w3schools.com>
2024-05-01 12:28:08 +0530

    second commit

--- Routes/api/v1/index.js
+++ Routes/api/v1/index.js
@@ -4,10 +4,10 @@ const uservalidatore = require("./validatore/validator");
 const middeleweare=require("./midleweare/middleweare")
 
 routv1.post("/user",uservalidatore.isvalidate,middeleweare.readdata,usercontroller.creatuser);
-routv1.get("/user",usercontroller.getalluser);
-routv1.patch("/user",usercontroller.updateuser);
-routv1.delete("/user",usercontroller.deleteuser);
-routv1.get("/user/:id",usercontroller.finduser);
+routv1.get("/user",middeleweare.readdata,usercontroller.getalluser);
+routv1.patch("/user",middeleweare.readdata,usercontroller.updateuser);
+routv1.delete("/user",middeleweare.readdata,usercontroller.deleteuser);
+routv1.get("/user/:id",middeleweare.readdata,usercontroller.finduser);
 
 routv1.all('*',function (req,res){
     return res.status(404).json({message:" Not Found"});
--- Routes/index.js
+++ Routes/index.js
@@ -7,4 +7,5 @@ rout.use('/api',apirout)
 rout.all('*',function (req,res){
     return res.status(404).json({message:"Not Found"});
 })
+// ghp_r6SZaPtOyrlE0CtZTqyH7Qze7ORSr83H5wG
 module.exports = rout
