0bf6143a09b0ca77db27b023994bfa5f24a44511
w3schools-test <test@w3schools.com>
2024-05-02 12:14:26 +0530

    chges of middleweare

--- index.js
+++ index.js
@@ -2,24 +2,29 @@ const express = require("express");
 const app = express();
 const route = require("./routes_admin_user");
 require("dotenv").config();
-const config=require("./config");
-const morgan=require("morgan");
-const fs=require("fs");
-const path=require("path")
-const helmet=require("helmet");
+const config = require("./config");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+const helmet = require("helmet");
 
-app.use(helmet({
-  crossOriginResourcePolicy: false,
-}));
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
 
-const  accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
- 
-app.use(morgan('dev',{ stream: accessLogStream }))
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
 
-app.use(express.json())
+app.use(morgan("dev", { stream: accessLogStream }));
+
+app.use(express.json());
 
 app.use("/", route);
 
 app.listen(config.PORT, () => {
   console.log("server is running .....");
-})
+});
--- routes_admin_user/api/v1/user/index.js
+++ routes_admin_user/api/v1/user/index.js
@@ -3,12 +3,13 @@ const usercontroller = require("./lib/controller");
 const uservalidatore = require("./lib/validator");
 const middeleweare=require("./lib/middleweare")
 
-routv1.post("/user",uservalidatore.isvalidate,middeleweare.readdata,usercontroller.creatuser);
-routv1.get("/user",middeleweare.readdata,usercontroller.getalluser);
-routv1.patch("/user",middeleweare.readdata,usercontroller.updateuser);
-routv1.delete("/user",middeleweare.readdata,usercontroller.deleteuser);
-routv1.get("/user/:id",middeleweare.readdata,usercontroller.finduser);
-routv1.post("/user/login",middeleweare.readdata,usercontroller.loginuser)
+routv1.use(middeleweare.readdata)
+routv1.post("/user",uservalidatore.isvalidate,usercontroller.creatuser);
+routv1.get("/user",usercontroller.getalluser);
+routv1.patch("/user",usercontroller.updateuser);
+routv1.delete("/user",usercontroller.deleteuser);
+routv1.get("/user/:id",usercontroller.finduser);
+routv1.post("/user/login",usercontroller.loginuser)
 
 
 
